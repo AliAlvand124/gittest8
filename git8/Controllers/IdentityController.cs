e4cbfd0291b74b0297c91b70d6f0508bbff3718d
alvand <alialvand.it@gmail.com>
2023-12-29 23:14:18 +0330

    add identity controller

---
+++ git8/Controllers/IdentityController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace git8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IdentityController : ControllerBase
+    {
+    }
+}
